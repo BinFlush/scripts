f597f4fbe52e51269b93c7381179ac8b40f800f0
Jakup Lutzen <jakup@lutzen.co>
2022-05-27 21:16:38 +0200

    added autorun checkbox, and shuffled things around

--- pymodoro.py
+++ pymodoro.py
@@ -55,7 +55,7 @@ class Pomodoro():
                 self.iterate()
             self.update()
    #         if self.progress > 0 or self.has_done_first_start == False:
-            #print("sum=", self.sum, "periods=", self.periods, "progress=", self.progress, "activity=", self.activity)
+            print("sum=", self.sum, "periods=", self.periods, "progress=", self.progress, "activity=", self.activity)
    #         self.has_done_first_start == True
             self.root.after(250,self.timer)
 
@@ -83,10 +83,12 @@ class Pomodoro():
             # odd number means work
             self.activity = self.work
             self.lb.configure(bg='red')
+            self.status.set('WORK')
             self.message = "STARTING WORKING SESSION"
         else:
             # break
             self.lb.configure(bg='green')
+            self.status.set('BREAK')
             if self.periods % 8 == 0:
                 # longbrake
                 self.activity = self.long
@@ -100,6 +102,12 @@ class Pomodoro():
         self.sum = self.activity
         if not self.count:
             self.update()
+            self.progress = 0
+        else:
+            if self.autorun.get() == 0:
+                self.progress = 0
+                self.pauseplay()
+            
 
 
 
@@ -125,26 +133,32 @@ class Pomodoro():
         self.root.title("Pymodoro")
 #        self.root.geometry("170x140")
         self.t = StringVar()
+        self.status = StringVar(value='waiting')
         self.cycle_t = StringVar()
+        self.autorun = IntVar(value=1)
         self.cycle_t.set(str(self.full_cycle))
 #        self.t.set("00:00:00")
         self.update()
-        self.lb = Label(self.root, textvariable=self.t, font=("Times 25 bold"), bg="white")
-        self.periods_label = Label(self.root, textvariable=self.cycle_t, font=("Times 18 bold"), bg="gray")
+        self.lb = Label(self.root, textvariable=self.t, font=("Times 22 bold"), bg="white")
+        self.statusfield = Label(self.root, textvariable=self.status, bg="#CCCCCC")
+        self.periods_label = Label(self.root, textvariable=self.cycle_t, font=("Times 18 bold"), bg="#CCCCCC")
         self.bt1 = Button(self.root, text="On/Off", command=self.pauseplay, font=("Times 12 bold"), bg=("gray"))
         self.bt3 = Button(self.root, text="Reset", command=self.reset, font=("Times 12 bold"), bg=("gray"))
         self.bt4 = Button(self.root, text="->", command=self.forward, font=("Times 12 bold"), bg=("gray"))
         self.bt5 = Button(self.root, text="<-", command=self.backward, font=("Times 12 bold"), bg=("gray"))
         self.bt6 = Button(self.root, text="->>", command=self.iterate, font=("Times 12 bold"), bg=("gray"))
+        self.auto = Checkbutton(self.root, text='auto',variable=self.autorun, onvalue=1, offvalue=0, bg='gray')
         self.root.bind("k",self.pauseplay)
 
-        self.lb.grid(row=0,columnspan=3, sticky='ew')# counter
-        self.periods_label.grid(row=2,column=0, sticky='ew')# counter
-        self.bt5.grid(row=1,column=0, sticky='ew')# back
+        self.lb.grid(row=0,column=1, sticky='ew')# main-counter
+        self.statusfield.grid(row=0,column=2, sticky='nsew')# status
+        self.periods_label.grid(row=0,column=0, sticky='nsew')# periods-counter
+        self.bt5.grid(row=1,column=0, sticky='nsew')# back
         self.bt1.grid(row=1,column=1, sticky='ew')# on/off
-        self.bt4.grid(row=1,column=2, sticky='ew')# forward
+        self.bt4.grid(row=1,column=2, sticky='nsew')# forward
         self.bt3.grid(row=2,column=1, sticky='ew')# reset
         self.bt6.grid(row=2,column=2, sticky='ew')# ->>
+        self.auto.grid(row=2,column=0, sticky='nsew') # checkbox
 #       self.label = Label(self.root,text="",font=("Times 40 bold"))
         self.root.configure(bg='#191919')
         self.root.bind('<space>', self.pauseplay)
